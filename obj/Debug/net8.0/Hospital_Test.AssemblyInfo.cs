427203a706039923d98a0ebe2dda91e1eff1f4fd
lemingha <lemigha@gmail.com>
2025-06-14 12:57:01 +0700

    sửa lại "Cập nhật sửa chữa"

--- obj/Debug/net8.0/Hospital_Test.AssemblyInfo.cs
+++ obj/Debug/net8.0/Hospital_Test.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital_Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e3bf5907aecbd7fc729577cc660192c387d3f43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad5814e7e1c0821fa85efcfd5b9e3e3a91dacb34")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital_Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital_Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
