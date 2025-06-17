271275f655a22819a7da57775c2b92dadc3f8087
nguyenAn2111 <nguyenquocanym@gmail.com>
2025-06-18 03:31:29 +0700

    test merge

--- obj/Debug/net8.0/Hospital_Test.AssemblyInfo.cs
+++ obj/Debug/net8.0/Hospital_Test.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital_Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+950bd9b94f2850e1a66578182b9d108550ada756")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efbade6ffc25d1bc0ed6aa1178cc04bbcf82d9e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital_Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital_Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
